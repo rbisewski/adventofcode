183fa72c0233f395a6004c47feb79c3e38300f3f
Robert Bisewski <contact@ibiscybernetics.com>
2017-12-21 11:12:47 -0600

    did a bit of work on Day 21

--- day21/day21.go
+++ day21/day21.go
@@ -49,4 +49,76 @@ func main() {
 	if len(lines) == 0 {
 		os.Exit(1)
 	}
+
+	// read in input
+	arrangements := make(map[string]string)
+	for _, line := range lines {
+
+		elements := strings.Split(line, " => ")
+
+		if len(elements) != 2 {
+			continue
+		}
+
+		key := strings.TrimSpace(elements[0])
+		value := strings.TrimSpace(elements[1])
+
+		arrangements[key] = value
+	}
+
+	// for every key, flip each of the input keys to account for all
+	// possibilities
+	for key, value := range arrangements {
+
+		keyParts := strings.Split(key, "\\")
+
+		// vertical flipped
+		verticalFlippedKey := ""
+		for _, elm := range keyParts {
+			verticalFlippedKey += verticalFlipString(elm) + "/"
+		}
+		verticalFlippedKey = strings.Trim(verticalFlippedKey, "/")
+		arrangements[verticalFlippedKey] = value
+
+		// 90 degrees flipped
+		nintyDegreesFlippedKey := nintyDegreeFlipString(keyParts)
+		arrangements[nintyDegreesFlippedKey] = value
+
+		// horizontal flipped
+		horizontalFlippedKey := ""
+		for _, elm := range keyParts {
+			horizontalFlippedKey = elm + "/" + horizontalFlippedKey
+		}
+		horizontalFlippedKey = strings.Trim(horizontalFlippedKey, "/")
+		arrangements[horizontalFlippedKey] = value
+	}
+
+	fmt.Println(arrangements)
+}
+
+func verticalFlipString(s string) string {
+
+	if len(s) < 2 {
+		return s
+	}
+
+	newString := ""
+
+	for i := len(s) - 1; i >= 0; i-- {
+		char := string(s[i])
+		newString += char
+	}
+
+	return newString
+}
+
+func nintyDegreeFlipString(elements []string) string {
+
+	if len(elements) < 1 {
+		return ""
+	}
+
+	newString := ""
+
+	return newString
 }
