f4a26ec5e4c1b0df5ae52c692352bf4d548c53f8
Robert Bisewski <contact@ibiscybernetics.com>
2024-12-12 19:43:36 -0600

    Day 12 part 2 solution

--- 2024/day12/main.py
+++ 2024/day12/main.py
@@ -42,6 +42,11 @@ def mapOutRegion(x, y, letter, grid, length, height):
 
     return tiles
 
+def resetGlobals():
+    global positionsRecorded, regions
+    positionsRecorded = set()
+    regions = {}
+
 def partOne(filename):
     total = 0
     grid = []
@@ -79,14 +84,55 @@ def partOne(filename):
     print(f"Part One: {total}")
 
 def partTwo(filename):
+    resetGlobals()
     total = 0
+    grid = []
+    length = 0
+    height = 0
 
     with open(filename) as fh:
         for line in fh:
             row = list(line.strip())
+            length = len(row)
+            height += 1
+            grid.append(row)
+
+    incrementer = 0
+    for y in range(height):
+        for x in range(length):
+            if (x, y) not in positionsRecorded:
+                letter = grid[y][x]
+                regions[f"{letter}-{incrementer}"] = mapOutRegion(x, y, letter, grid, length, height)
+                incrementer += 1
+
+    for r in regions:
+        area = len(regions[r])
+
+        perimeter = set()
+        for x, y in regions[r]:
+            for n in getNeighbours([x,y]):
+                nx = n[0]
+                ny = n[1]
+                if (nx, ny) not in regions[r]:
+                    perimeter.add(((x,y),(nx,ny)))
+
+        sides = set()
+        for pos1, pos2 in perimeter:
+            x1 = pos1[0]
+            y1 = pos1[1]
+            x2 = pos2[0]
+            y2 = pos2[1]
+            if ((x1+1, y1),(x2+1, y2)) in perimeter:
+                continue
+            elif ((x1, y1+1),(x2, y2+1)) in perimeter:
+                continue
+            else:
+                sides.add((pos1, pos2))
+
+        total += area * len(sides)
 
     print(f"Part Two: {total}")
 
 # ---
 partOne("input1.txt")
-partTwo("input0.txt")
+partTwo("input1.txt")
