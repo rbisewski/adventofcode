385f61a3a5b673622106693535b8e966fc48bf01
Robert Bisewski <contact@ibiscybernetics.com>
2024-12-13 15:58:25 -0600

    Year 2024 - Day 13 part 2 solution

--- 2024/day13/main.py
+++ 2024/day13/main.py
@@ -1,8 +1,7 @@
 import re
 
-input = []
-
 def partOne(filename):
+    input = []
     total = 0
 
     with open(filename) as fh:
@@ -84,20 +83,86 @@ def partOne(filename):
     print(f"Part One: {total}")
 
 def partTwo(filename):
+    input = []
     total = 0
-    grid = []
-    length = 0
-    height = 0
 
     with open(filename) as fh:
+        buttonA = (0,0)
+        buttonB = (0,0)
+        prize = (0,0)
         for line in fh:
-            row = list(line.strip())
-            length = len(row)
-            height += 1
-            grid.append(row)
+            aMatches = re.findall(r"Button A: X\+([0-9]+), Y\+([0-9]+)",line)
+            if aMatches:
+                x = int(aMatches[0][0])
+                y = int(aMatches[0][1])
+                buttonA = (x,y)
+                continue
+
+            bMatches = re.findall(r"Button B: X\+([0-9]+), Y\+([0-9]+)",line)
+            if bMatches:
+                x = int(bMatches[0][0])
+                y = int(bMatches[0][1])
+                buttonB = (x,y)
+                continue
+
+            pMatches = re.findall(r"Prize: X\=([0-9]+), Y\=([0-9]+)",line)
+            if pMatches:
+                x = int(pMatches[0][0])
+                y = int(pMatches[0][1])
+                prize = (x,y)
+                input.append({
+                    "Button A": buttonA,
+                    "Button B": buttonB,
+                    "Prize": prize
+                })
+                continue
+
+    # Button A costs 3 tokens
+    # Button B costs 1 token
+    allPrizes = []
+    for i in input:
+        ax = i["Button A"][0]
+        ay = i["Button A"][1]
+
+        bx = i["Button B"][0]
+        by = i["Button B"][1]
+
+        px = i["Prize"][0] + 10000000000000
+        py = i["Prize"][1] + 10000000000000
+
+        prizeData = {
+            "A Pushes": -1,
+            "B Pushes": -1,
+            "Cost": -1
+        }
+
+        # first, figure out the value of A
+        numerator = abs((by * px) - (bx * py))
+        denominator = abs((bx * (ay + by)) - (by * (ax + bx)))
+
+        aValue = numerator // denominator
+
+        # second, figure out the value of B
+        bValue = (px - (ax * aValue)) // bx
+
+        # third, use modulo to check if any remainders; if so, then the prize can't be reached
+        aRemainders = numerator % denominator
+        bRemainders = (px - (ax * aValue)) % bx
+
+        if aRemainders > 0 or bRemainders > 0:
+            continue
+
+        prizeData["A Pushes"] = aValue
+        prizeData["B Pushes"] = bValue
+        prizeData["Cost"] = (aValue * 3) + (bValue)
+
+        allPrizes.append(prizeData)
+
+    for p in allPrizes:
+        total += p["Cost"]
 
     print(f"Part Two: {total}")
 
 # ---
 partOne("input1.txt")
-partTwo("input0.txt")
+partTwo("input1.txt")
